ad3d5cc8c44125eb7b2edc459eebd98eb3404e8f
clickingmouse <clickingmouse@users.noreply.github.com>
2019-10-29 18:56:34 +0800

    npx react & basic ws backend

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/clickingmouse/t1/game-of-life
 
 go 1.13
+
+require github.com/gorilla/websocket v1.4.1
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+//upgrader
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	//CORS
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+//reader - listen to new messages to ws
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// print out msg for clarity
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
+}
+
+//ws endpoint
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	//upgrade connection to ws connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	// listen indefinitely for msgs coming in
+	reader(ws)
+
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+}
 
 func main() {
-	fmt.Println("GOF v0.01")
+	//	fmt.Println("GOF v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
